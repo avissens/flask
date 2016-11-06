fcf4e6ad6f527bc4613c775a0911bdaa8af4776e
avissens <aviss@hotmail.co.uk>
2016-11-06 10:49:05 +0000

    Initial commit

---
+++ hello_world.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from os import environ
+
+app = Flask(__name__)
+
+@app.route("/jedi/<firstname>/<secondname>")
+def hi_person(firstname, secondname):
+    name = secondname[0:3] + firstname[0:2]
+    return "Hello {}!".format(name.title())
+
+if __name__ == "__main__":
+    app.run(host=environ['IP'],
+            port=int(environ['PORT']))
+
